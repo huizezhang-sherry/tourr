0b0f5e8135cbdbe03b12617e6649ad8080d097e0
Sherry <huizezhangsh@gmail.com>
2020-03-08 16:27:51 +1100

    clean the factor order after computation

--- sherry/2-alpha.R
+++ sherry/2-alpha.R
@@ -83,6 +83,26 @@ geodesic_alpha2 <- foreach(i = 1:length(stepS2), .combine = "rbind") %do%{
   compute_geodesic_alpha(stepS[i])
 }
 
+################################
+# clean the info
+
+clean_info <- function(data){
+  data %>%
+    mutate(info = fct_relevel(info, c("start",
+                                      "direction_search", "best_direction_search",
+                                      "line_search", "best_line_search",
+                                      "interpolation")),
+           info2 = as.factor(ifelse(info %in% c("best_direction_search","best_line_search"),
+                          "best", "normal")),
+           info3 = ifelse(info2 == "normal", as.character(info),
+                          str_sub(info, start = 6L, end = -1L)))
+}
+
+geodesic_alpha <- geodesic_alpha %>% clean_info()
+geodesic_alpha2 <- geodesic_alpha2 %>% clean_info()
+
+
+
 
 ################################
 
