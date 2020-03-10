447adc2bb40c7f48a352723535e23eb97b3ff5e0
Sherry <huizezhangsh@gmail.com>
2020-03-10 17:47:39 +1100

    simulate different eps value to see difference between better and better_random

---
+++ sherry/5-better_random_eps.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(foreach)
+library(doFuture)
+registerDoFuture()
+plan(multicore)
+
+################################
+load(here::here("sherry", "data", "data.rda"))
+
+
+# functions to use
+compute_random_eps<- function(eps, cooling){
+
+  animate_dist(data, tour_path =
+                 guided_tour(holes(), d = 1, eps = eps, alpha = 0.5,
+                             cooling = cooling,
+                             search_f = search_better_random),
+               sphere = TRUE) %>%
+    mutate(eps = eps,
+           cooling = cooling,
+           alpha = 0.5,
+           method = "search_better_random")
+}
+
+temp <- compute_random_eps(0.001, 0.95)
+#eps <- c(0.001,0.01, 0.05, 0.1, 0.5)
+eps <- c(1e-5, 5e-5, 1e-4, 5e-4, 0.001)
+cooling <- c(0.95, 0.99, 0.995)
+paras <- expand.grid(eps, cooling)
+
+better_random_eps <- foreach(i = 1:nrow(paras), .combine = "rbind") %do%{
+  set.seed(123456)
+  compute_random_eps(paras$Var1[i], paras$Var2[i])
+}
+
+# save(better_random_eps, file = "sherry/data/better_random_eps.rda")
+
--- sherry/compute_pca.R
+++ sherry/compute_pca.R
@@ -65,3 +65,26 @@ pca_better_interrupt <- function(data){
 
 }
 
+
+pca_better_eps <- function(data){
+
+  pca <- foreach::foreach(i  = 1:nrow(data), .combine = "rbind") %do%{
+    t(data$basis[[i]])
+  }
+
+  loadings <- stats::predict(stats::prcomp(pca))[,1:2]
+  pca2 <- cbind(pca, loadings)
+
+  result <- as_tibble(pca2) %>% mutate(id = 1: nrow(pca2),
+                                       info = data$info,
+                                       tries = data$tries,
+                                       index_val = data$index_val,
+                                       alpha = data$alpha,
+                                       cooling = data$cooling,
+                                       eps = data$eps)
+
+  colnames(result)[1:5] <- c("x1", "x8", "x9", "x10", "x2")
+
+  return(result)
+
+}
