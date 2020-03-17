df81f0413f73b9fa1c6f293d5a05a8de9f531661
Sherry <huizezhangsh@gmail.com>
2020-03-17 23:14:12 +1100

    simulate with 2 informative vars: x2 & x7

--- sherry/6-mult.R
+++ sherry/6-mult.R
@@ -1,95 +1,55 @@
 library(tidyverse)
 library(foreach)
+source(here::here("sherry", "cleaning.R"))
 
 set.seed(1234)
 x1 <- rnorm(1000, 0, 1)
-x2 <- c(rnorm(500, -3, 1), rnorm(500, 3, 1))
-X7 <- c(rnorm(500, -5, 1), rnorm(500, 5, 1))
+x2 <- sample(c(rnorm(500, -3, 1), rnorm(500, 3, 1)), size = 1000)
+x7 <- sample(c(rnorm(500, -5, 1), rnorm(500, 5, 1)), size = 1000)
 x8 <- rnorm(1000, 0, 1)
 x9 <- rnorm(1000, 0, 1)
 x10 <- rnorm(1000, 0, 1)
 
 data_mult <- cbind(x1, x2, x7, x8, x9, x10)
 
-temp <- data_mult %>% scale()
+# scaling is helpful for pp to find the optimal projection
+data_mult_scaled <- data_mult%>% scale()
 
-set.seed(123456)
-a <- tourr::animate_dist(temp, tour_path = guided_tour(holes(), d = 1,
-                                                       search_f = search_geodesic_latest),
-                         sphere = FALSE, rescale = FALSE)
 
 ################################
-set.seed(123456)
-mult_geodesic <- animate_dist(data_mult, tour_path =
-                           guided_tour(holes(), d = 1,
-                                       search_f = search_geodesic_latest),
-                         sphere = FALSE, rescale = FALSE) %>%
-    mutate(col = "x7") %>%
-    mutate(method = "geodesic")
-
-set.seed(123456)
-mult_better <- animate_dist(data_mult, tour_path =
-                                guided_tour(holes(), d = 1,
-                                            search_f = search_better),
-                              sphere = FALSE, rescale = FALSE) %>%
-  mutate(col = "x7") %>%
-  mutate(method = "better")
-
-set.seed(123456)
-mult_better_random <- animate_dist(data_mult, tour_path =
-                                guided_tour(holes(), d = 1,
-                                            search_f = search_better_random),
-                              sphere = FALSE, rescale = FALSE) %>%
-  mutate(col = "x7") %>%
-  mutate(method = "better_random")
+# when having two informative variables - one-dimensional projection would find either one of the informative variable
+# set.seed(12345)
+# a <- tourr::animate_dist(data_mult_scaled,
+#                          tour_path = guided_tour(holes(), d = 1,
+#                                                  search_f = search_geodesic_latest),
+#                          sphere = FALSE, rescale = FALSE)
+#
+# proj <- a %>% filter(info == "interpolation") %>%  pull(basis) %>% tail(1)
+# projected <- data_mult_scaled %*% proj[[1]] %>% as_tibble()
+#
+# projected %>%
+#   ggplot(aes(x = V1)) +
+#   geom_histogram(binwidth = 0.1) +
+#   geom_density(aes(y=0.1 * ..count..))
 
 ################################
-compute_pca <- function(data, names){
-  rows <- data %>% filter(col == names)
-  pca <- foreach::foreach(i  = 1:nrow(rows), .combine = "rbind") %do%{
-    t(rows$basis[[i]])
-  }
+# two dimensional projection
+set.seed(12345)
+mult_geodesic <- tourr::animate_xy(data_mult_scaled,
+                          tour_path = guided_tour(holes(), d = 2,
+                                                  search_f = search_geodesic_latest),
+                          sphere = FALSE, rescale = FALSE)
 
-  loadings <- stats::predict(stats::prcomp(pca))[,1:2]
-  pca2 <- cbind(pca, loadings)
-
-  result <- as_tibble(pca2) %>% mutate(id = 1: nrow(pca2),
-                                       info = rows$info,
-                                       V5_names = rows$col,
-                                       tries = rows$tries,
-                                       loop = rows$loop,
-                                       index_val = rows$index_val)
-
-  colnames(result)[1:4] <- c("x1", "x8", "x9", "x10")
-
-  return(result)
-
+pca <- foreach::foreach(i  = 1:nrow(mult_geodesic), .combine = "rbind") %do%{
+  t(c(mult_geodesic$basis[[i]]))
 }
+loadings <- stats::predict(stats::prcomp(pca))[,1:2]
+pca2 <- cbind(pca, loadings)
 
-mult_object <- mult_geodesic %>% compute_pca("x7") %>%
-  mutate(info = fct_relevel(info, c("start", "direction_search",
-                                    "best_direction_search", "line_search",
-                                    "best_line_search")))
-
-mult_object_better <- mult_better %>% compute_pca("x7") %>%
-  mutate(info = fct_relevel(info, c("start", "random_search", "new_basis")))
-
-
-mult_object_better_random <- mult_better_random %>% compute_pca("x7") %>%
-  mutate(info = fct_relevel(info, c("start", "random_search", "new_basis")))
+mult_object <- as_tibble(pca2) %>% bind_cols(mult_geodesic) %>% clean_info()
 
 
 ################################
 save(data_mult,file = "sherry/data/data_mult.rda")
 save(mult_geodesic, file = "sherry/data/mult_geodesic.rda")
-save(mult_better, file = "sherry/data/mult_better.rda")
-save(mult_better_random, file = "sherry/data/mult_better_random.rda")
-
 save(mult_object, file = "sherry/data/mult_object.rda")
-save(mult_object_better, file = "sherry/data/mult_object_better.rda")
-save(mult_object_better_random, file = "sherry/data/mult_object_better_random.rda")
-
-
-
-
-
