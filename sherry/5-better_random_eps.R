21ee3683f80821156d1e785dc8161084969c4948
Sherry Zhang <huizezhangsh@gmail.com>
2020-03-23 14:29:40 +1100

    fix variance = 1 for data_mult

--- sherry/5-better_random_eps.R
+++ sherry/5-better_random_eps.R
@@ -4,12 +4,11 @@ library(doFuture)
 registerDoFuture()
 plan(multicore)
 
-################################
 load(here::here("sherry", "data", "data.rda"))
 
-
+################################
 # functions to use
-compute_random_eps<- function(eps, cooling){
+compute_random_eps <- function(eps, cooling){
 
   animate_dist(data, tour_path =
                  guided_tour(holes(), d = 1, eps = eps, alpha = 0.5,
@@ -22,20 +21,18 @@ compute_random_eps<- function(eps, cooling){
            method = "search_better_random")
 }
 
-temp <- compute_random_eps(0.001, 0.95)
+################################
 #eps <- c(0.001,0.01, 0.05, 0.1, 0.5)
-<<<<<<< HEAD
 eps <- c(1e-5, 5e-5, 1e-4, 5e-4, 0.001)
-=======
-eps <- c(1e-5, 5e-5, 1e-4, 5e-4, 0.001, 0005, 0.01)
->>>>>>> b1bb63224cd535e2df6e698abff890e99409a36f
 cooling <- c(0.95, 0.99, 0.995)
 paras <- expand.grid(eps, cooling)
 
-better_random_eps <- foreach(i = 1:nrow(paras), .combine = "rbind") %do%{
+################################
+better_random_eps <- foreach(i = 1:nrow(paras), .combine = "rbind") %do% {
   set.seed(123456)
   compute_random_eps(paras$Var1[i], paras$Var2[i])
 }
 
-# save(better_random_eps, file = "sherry/data/better_random_eps.rda")
+################################
+save(better_random_eps, file = "sherry/data/better_random_eps.rda")
 
--- sherry/6-mult.R
+++ sherry/6-mult.R
@@ -10,46 +10,38 @@ x8 <- rnorm(1000, 0, 1)
 x9 <- rnorm(1000, 0, 1)
 x10 <- rnorm(1000, 0, 1)
 
-data_mult <- cbind(x1, x2, x7, x8, x9, x10)
-
-# scaling is helpful for pp to find the optimal projection
-data_mult_scaled <- data_mult%>% scale()
+data_mult <- tibble::tibble(x1 = x1, x2 = x2, x7 = x7,
+                       x8 = x8, x9 = x9, x10 = x10) %>% map_df(scale)
 
 
 ################################
 # when having two informative variables - one-dimensional projection would find either one of the informative variable
+# A two dimensional projection will be better!
 set.seed(12345)
-a <- tourr::animate_dist(data_mult_scaled,
-                         tour_path = guided_tour(holes(), d = 1,
-                                                 search_f = search_geodesic_latest),
-                         sphere = FALSE, rescale = FALSE)
+mult_geodesic <- animate_xy(data_mult,tour_path =
+                              guided_tour(holes(), d = 2,
+                                          search_f = search_geodesic_latest),
+                            rescale = FALSE)
 
-proj <- a %>% filter(info == "interpolation") %>%  pull(basis) %>% tail(1)
-projected <- data_mult_scaled %*% proj[[1]] %>% as_tibble()
-
-projected %>%
-  ggplot(aes(x = V1)) +
-  geom_histogram(binwidth = 0.1) +
-  geom_density(aes(y=0.1 * ..count..))
-
-################################
-# two dimensional projection
-set.seed(12345)
-mult_geodesic <- tourr::animate_xy(data_mult_scaled,
-                          tour_path = guided_tour(holes(), d = 2,
-                                                  search_f = search_geodesic_latest),
-                          sphere = FALSE, rescale = FALSE)
-
-pca <- foreach::foreach(i  = 1:nrow(mult_geodesic), .combine = "rbind") %do%{
+pca <- foreach::foreach(i  = 1:nrow(mult_geodesic), .combine = "rbind") %do% {
   t(c(mult_geodesic$basis[[i]]))
 }
-loadings <- stats::predict(stats::prcomp(pca))[,1:2]
-pca2 <- cbind(pca, loadings)
+loadings <- stats::predict(stats::prcomp(pca[,1:6]))[,1:2]
+pca_v1 <- cbind(pca[,1:6], loadings) %>%
+  as_tibble() %>%
+  bind_cols(mult_geodesic) %>%
+  clean_info()
 
-mult_object <- as_tibble(pca2) %>% bind_cols(mult_geodesic) %>% clean_info()
+loadings <- stats::predict(stats::prcomp(pca[,7:12]))[,1:2]
+pca_v2 <- cbind(pca[,7:12], loadings) %>%
+  as_tibble() %>%
+  bind_cols(mult_geodesic) %>%
+  clean_info()
+
 
 
 ################################
 save(data_mult,file = "sherry/data/data_mult.rda")
 save(mult_geodesic, file = "sherry/data/mult_geodesic.rda")
-save(mult_object, file = "sherry/data/mult_object.rda")
+save(pca_v1, file = "sherry/data/pca_v1.rda")
+save(pca_v2, file = "sherry/data/pca_v2.rda")
